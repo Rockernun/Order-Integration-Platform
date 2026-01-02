26c358f90858452e6884aec9e669eb46c377ff97
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2026-01-02 14:47:58 +0900

    feat(Controller): Store 생성/조회 API 추가

---
+++ src/main/java/order_system/pickup/store/StoreController.java
@@ -0,0 +1,35 @@
+package order_system.pickup.store;
+
+import jakarta.validation.Valid;
+import order_system.pickup.store.dto.StoreCreateRequest;
+import order_system.pickup.store.dto.StoreResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/stores")
+public class StoreController {
+
+    private final StoreService storeService;
+
+    public StoreController(StoreService storeService) {
+        this.storeService = storeService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public StoreResponse createStore(@RequestBody @Valid StoreCreateRequest req) {
+        return storeService.createStore(req);
+    }
+
+    @GetMapping("/{storeId}")
+    public StoreResponse getStore(@PathVariable Long storeId) {
+        return storeService.getStore(storeId);
+    }
+}
