72dfe716af4d4382da2072a3c987d7ad97faf846
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2026-01-02 16:02:54 +0900

    feat(Exception): 존재하지 않는 id를 가진 Store에 대한 예외 객체 추가
    - StoreService에서 StoreNotFoundException() 예외로 공통 처리

---
+++ src/main/java/order_system/pickup/store/StoreNotFoundException.java
@@ -0,0 +1,7 @@
+package order_system.pickup.store;
+
+public class StoreNotFoundException extends RuntimeException {
+    public StoreNotFoundException(Long id) {
+        super("Store 아이디가 " + id + "인 Store는 찾을 수 없습니다.");
+    }
+}
--- src/main/java/order_system/pickup/store/StoreService.java
+++ src/main/java/order_system/pickup/store/StoreService.java
@@ -16,11 +16,11 @@ public class StoreService {
     public StoreResponse createStore(StoreCreateRequest req) {
         Long storeId = storeRepository.save(req);
         return storeRepository.findById(storeId)
-                .orElseThrow(() -> new IllegalStateException("해당 id를 가진 Store를 찾을 수 없습니다."));
+                .orElseThrow(() -> new StoreNotFoundException(storeId));
     }
 
     public StoreResponse getStore(Long storeId) {
         return storeRepository.findById(storeId)
-                .orElseThrow(() -> new IllegalStateException("해당 id를 가진 Store를 찾을 수 없습니다."));
+                .orElseThrow(() -> new StoreNotFoundException(storeId));
     }
 }
