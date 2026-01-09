8d8510b0fe1ef54d6a3779b466c3c6e473d0af08
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2026-01-09 15:50:54 +0900

    style(Exception): 주문 정보가 존재하지 않을 경우에 대한 예외 처리 메시지 수정

--- src/main/java/order_system/pickup/order/OrderNotFoundException.java
+++ src/main/java/order_system/pickup/order/OrderNotFoundException.java
@@ -2,6 +2,6 @@ package order_system.pickup.order;
 
 public class OrderNotFoundException extends RuntimeException {
     public OrderNotFoundException(Long orderId) {
-        super("주문이 생성되었으나 조회되지 않습니다: " + orderId);
+        super("orderId: " + orderId + "에 대한 주문 정보를 찾을 수 없습니다.");
     }
 }
