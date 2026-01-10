b6145f8829a62fb2da0f60ec50782ca84cf7082a
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2026-01-10 18:40:46 +0900

    feat(Constant): outbox 이벤트 타입 상수 처리

---
+++ src/main/java/order_system/pickup/outbox/constant/AggregateType.java
@@ -0,0 +1,8 @@
+package order_system.pickup.outbox.constant;
+
+public final class AggregateType {
+
+    private AggregateType() {}
+
+    public static final String ORDER = "ORDER";
+}
---
+++ src/main/java/order_system/pickup/outbox/constant/OutboxEventType.java
@@ -0,0 +1,8 @@
+package order_system.pickup.outbox.constant;
+
+public final class OutboxEventType {
+
+    private OutboxEventType() {}
+
+    public static final String ORDER_CREATED = "ORDER_CREATED";
+}
