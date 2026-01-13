8ef93270e19deb5a42f0a297036d8691cfb47f39
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2026-01-13 16:59:53 +0900

    feat(Mock): 파트너 Mock 주문 수신 엔드포인트 추가

---
+++ src/main/java/order_system/pickup/partner/mock/PartnerMockFailureException.java
@@ -0,0 +1,7 @@
+package order_system.pickup.partner.mock;
+
+public class PartnerMockFailureException extends RuntimeException {
+    public PartnerMockFailureException(String message) {
+        super(message);
+    }
+}
